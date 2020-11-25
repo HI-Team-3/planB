89f8d986ee6a7efcfd17bf463dcda408f63758e2
SpikerSpiegel <alestry.perez@hyperisland.se>
2020-11-25 15:36:31 +0100

    Gifs working

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 // API KEY: fptljKSZwqx1cVFvU9lcEk0yQxxXC3UU
+
+const index = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+
+const random = Math.floor(Math.random() * index.length);
+
+//This is a general API template that you can use with many of the API's you will find on the web.
+
+//create variable that store button in javascript variable
+let button = document.querySelector("#getData");
+
+//add an event listener to the button. Run the function sendApiRequest when the button is clicked
+button.addEventListener("click", () => {
+  sendApiRequest();
+});
+
+//fetch data from the API
+async function sendApiRequest() {
+  let response = await fetch(
+    `https://api.giphy.com/v1/gifs/random?api_key=fptljKSZwqx1cVFvU9lcEk0yQxxXC3UU&tag=&rating=g`
+  );
+  console.log(response);
+  let gifs = await response.json();
+  console.log(gifs);
+  useApiData(gifs);
+}
+
+//do something with the API data you've received.
+function useApiData(gifs) {
+  document.querySelector(
+    "#wrapper"
+  ).innerHTML = `<img src = "${gifs.data.images.original.url}">`;
+}
