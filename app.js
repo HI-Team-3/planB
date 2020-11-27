a8840ab09a59353bad2f9a2e6a5011eaabc9f423
Alestry Pérez <alestry.perez@hyperisland.se>
2020-11-27 02:03:41 +0100

    js changes

--- app.js
+++ app.js
@@ -1,32 +1,51 @@
 //create javascript variable
 let button = document.querySelector("#getData");
-var gifBoxes = document.querySelector("#wrapper");
+var gifBox = document.querySelector("#wrapper");
 var soundApiKey = `iUEo8G4SpNyoDuuhogZQMnnNDuHTyco8Ia1pcRXC`;
 var giphyApiKey = "fptljKSZwqx1cVFvU9lcEk0yQxxXC3UU";
-var giphyRandomURL = `https://api.giphy.com/v1/gifs/random?api_key=${giphyApiKey}&tag=random&rating=r`;
-var soundURL = `https://freesound.org/apiv2/users/LimitSnap_Creations/sounds/?query=piano&token=${soundApiKey}`; //not working
-console.log(soundURL);
+var giphyRandomURL = `https://api.giphy.com/v1/gifs/random?api_key=${giphyApiKey}&tag=funny&rating=r`;
+var soundURL = `https://freesound.org/apiv2/sounds/279004/?token=${soundApiKey}`;
 window.onload = function () {
+  soundManager.setup({
+    // where to find the SWF files, if needed
+    url: soundURL,
+    onready: function () {
+      // SM2 has loaded, API ready to use e.g., createSound() etc.
+    },
+    ontimeout: function () {
+      // Uh-oh. No HTML5 support, SWF missing, Flash blocked or other issue
+    },
+  });
   //add an event listener to the button. Run the function sendApiRequest when the button is clicked
   button.addEventListener("click", () => {
     sendApiRequest();
+    sendSoundApiRequest();
   });
   //fetch data from the API
   async function sendApiRequest() {
     let response = await fetch(giphyRandomURL);
-    console.log(response);
     let gifs = await response.json();
-    console.log(gifs);
     useApiData(gifs);
+  }
+  async function sendSoundApiRequest() {
+    let response = await fetch(soundURL);
+    let sounds = await response.json();
+    useSoundApiData(sounds);
   }
   //do something with the API data you've received.
   function useApiData(gifs) {
-    gifBoxes.innerHTML = `<img class="bubble" src = "${gifs.data.images.original.url}">`;
+    gifBox.innerHTML = `<img class="bubble" src = "${gifs.data.images.original.url}">`;
     anime({
       targets: ".bubble",
       scale: 2.5,
       rotate: "3turn",
       easing: "spring(1, 80, 100, 0)",
     });
+  }
+  function useSoundApiData(sounds) {
+    var mySound = soundManager.createSound({
+      url: 
+     });
+     mySound.play();//not working
   }
 };
