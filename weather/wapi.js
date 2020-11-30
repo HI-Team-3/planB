9508ae931686868b0ffa6e4f0e91e10915e30706
SpikerSpiegel <alestry.perez@hyperisland.se>
2020-11-30 16:04:52 +0100

    Collab Changes

--- weather/wapi.js
+++ weather/wapi.js
@@ -1,9 +1,9 @@
-let city = "kiruna";
+let city = "stockholm";
 let temperatureDegree = document.querySelector(".temperature-degree");
 let button = document.querySelector(".btn");
-const apikey = "87c18c31ffa6fe9c3b68cb4d2bd50b14";
+const weatherApikey = "87c18c31ffa6fe9c3b68cb4d2bd50b14";
 const proxy = `https://cors-anywhere.herokuapp.com/`;
-const api = `${proxy}api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+const api = `${proxy}api.openweathermap.org/data/2.5/weather?q=${city}&appid=${weatherApikey}`;
 
 button.addEventListener("click", () => {
   sendApiRequest();
